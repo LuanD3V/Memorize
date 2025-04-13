2669277a06726d4fd985dce14e7a5173707092aa
LuanD3V <luan.h.felipe@icloud.com>
2025-04-13 16:31:37 -0300

    feat: Criando CardView como exemplo para Cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
-            Text("👻").font(.largeTitle)
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: false)
         }
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool =  false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12).foregroundStyle(.black)
+            }
+        })
         .padding()
     }
 }
